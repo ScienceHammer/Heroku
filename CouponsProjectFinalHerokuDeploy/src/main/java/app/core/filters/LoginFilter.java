8d839d7b59287b9d028eaef6f865cb3193e7262c
nur haj <nor.alden@gmail.com>
2021-06-25 10:44:45 +0300

    ccccc

--- CouponsProjectFinalHerokuDeploy/src/main/java/app/core/filters/LoginFilter.java
+++ CouponsProjectFinalHerokuDeploy/src/main/java/app/core/filters/LoginFilter.java
@@ -34,7 +34,6 @@ public class LoginFilter implements Filter {
 		this.loginManager = loginManager;
 	}
 
-	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
